f9a61b312d03d7b2d7663ab1327f13c0ab85eaa9
Natã Oliveira <natanoliveira1100@gmail.com>
2025-10-17 00:59:55 -0300

    visibilidade do tema melhorada

--- wezterm.lua
+++ wezterm.lua
@@ -16,43 +16,38 @@ local config = wezterm.config_builder()
 
 
 config.color_scheme = "darkmatrix"
--- config.color_scheme = "darkmatrix"
 
-
--- Aumentar contraste/brilho do Darkmatrix
 config.colors = {
-    foreground = "#5a8020",  -- Verde mais brilhante (era #3e5715)
-    background = "#070c0e",   -- Mantém fundo escuro
-    cursor_bg = "#c5d98e",    -- Cursor mais brilhante (era #9fa86e)
-    cursor_border = "#c5d98e",
-    cursor_fg = "#00ff87",    -- Mantém
-    selection_bg = "#1a2b30",  -- Seleção mais visível (era #0f191c)
+    foreground = "#7cbb2e", 
+    background = "#070c0e",
+    cursor_bg = "#d4e8a0",
+    cursor_border = "#d4e8a0",
+    cursor_fg = "#00ff87",
+    selection_bg = "#1f3a42",
     selection_fg = "#00ff87",
     
-    -- Cores ANSI mais brilhantes
     ansi = {
-        "#0d1a1f",  -- black mais claro
-        "#00854d",  -- green mais brilhante (era #006536)
-        "#8fd966",  -- green claro mais brilhante (era #6fa64c)
-        "#a0a600",  -- yellow mais brilhante (era #7e8000)
-        "#3cc4a8",  -- cyan mais brilhante (era #2c9a84)
-        "#6e4d85",  -- magenta mais brilhante (era #523a60)
-        "#1a6b73",  -- cyan escuro mais brilhante (era #114d53)
-        "#00854d",  -- white (green)
+        "#1a2a2f",
+        "#00b366",
+        "#b0ff85",
+        "#d4d400",
+        "#52e8d0",
+        "#8f66a8",
+        "#2a8a95",
+        "#00b366",
     },
     
     brights = {
-        "#5a5a5a",  -- bright black mais claro
-        "#00a352",  -- bright green mais brilhante
-        "#b8ff8a",  -- bright green claro MUITO mais brilhante
-        "#ffff66",  -- bright yellow MUITO mais brilhante
-        "#66ffe0",  -- bright cyan MUITO mais brilhante
-        "#7a5088",  -- bright magenta
-        "#1a7a85",  -- bright cyan escuro
-        "#00a352",  -- bright white (green)
+        "#808080",
+        "#00e680",
+        "#d4ffb0",
+        "#ffff99",
+        "#99fff2",
+        "#b080d0",
+        "#33b0c0",
+        "#00e680",
     },
 }
-
 
 -- Disable tab bar for cleaner interface
 -- Set to true if you want to use WezTerm's built-in tab management
